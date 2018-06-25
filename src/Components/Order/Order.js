d39bd005735c6c1a38d6ee71098ea4316544d22c
cmdmescall <cmdmescall@gmail.com>
2018-06-25 16:50:07 +0100

    Basic logic for toggling Orders

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import './Order.css';
 
 class Order extends Component {
+  switchToCoffee = () => {
+    let coffee = document.querySelector('.firstPane');
+    let pastry = document.querySelector('.secondPane');
+    pastry.classList.add('hidden');
+    coffee.classList.remove('hidden');
+  }
+  switchToPastry = () => {
+    let coffee = document.querySelector('.firstPane');
+    let pastry = document.querySelector('.secondPane');
+    coffee.classList.add('hidden');
+    pastry.classList.remove('hidden');
+  }
   render() {
     return (
       <div className="order">
         <div className="tabs" id="coffees">
-          <button onclick="">Coffee</button>
+          <button onClick={this.switchToCoffee}>Coffee</button>
         </div>
 
         <div className="tabs" id="pastry">
-          <button onclick="">Pastry</button>
+          <button onClick={this.switchToPastry}>Pastry</button>
         </div>
+
+        <div className="firstPane">First Pane - Coffees</div>
+        <div className="secondPane hidden">Second Pane - Pastries</div>
       </div>
     );
   }
