e3e62118f31d4da2bb215431d0ca0e3ce205e90d
Matt <mattk.gt@gmail.com>
2018-06-25 15:55:08 +0100

    made initial changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Order from './Components/Order/Order';
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Roast Well Coffee Co</h1>
+        <Order />
       </div>
     );
   }
