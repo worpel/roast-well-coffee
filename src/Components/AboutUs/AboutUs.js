66c7a0860003b93cd745a4fe1912751e0714ffb4
Siân Rachel <sianracheldanforth@gmail.com>
2018-06-25 17:11:55 +0100

    about us section

---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './AboutUs.css';
+
+class AccountInfo extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>About Us</h1>
+            </div>
+        );
+    }
+}
+
+export default AboutUs;
