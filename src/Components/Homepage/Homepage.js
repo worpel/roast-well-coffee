1931752ad4760c6e4d617b67859a3d46d63d3c71
Siân Rachel <sianracheldanforth@gmail.com>
2018-06-25 16:17:42 +0100

    background divs

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './Homepage.css';
+import Background from '../../Images/background2';
 
-class Homepage extends Component {
-  render() {
-    return (
-      <div className="Homepage" style={{ backgroundimage: `url$(./Images{background2.jpg}`) } >
-        <h1>Homepage</h1>
-      </ div>
-        );
-      }
-    }
-    
-    export default Homepage;
+
+const Homepage = () => {
+  return (
+    <div className="mw5 mw7-ns center bg-light-gray pa3 ph5-ns">
+      <img src={Background} alt="background image" />
+      <h1>Homepage</h1>
+    </div>
+  );
+}
+
+
+
+export default Homepage;
