f31a695a1793d63e8f8f9cfb8c0293837524a6f1
cmdmescall <cmdmescall@gmail.com>
2018-06-25 15:55:12 +0100

    Basic Navbar Text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import tachyons from 'tachyons';
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Roast Well Coffee Co</h1>
+      <div>
+        <Navbar />
       </div>
     );
   }
