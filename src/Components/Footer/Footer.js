24d2799ee94631734af27b43e25b142460d61d3c
cmdmescall <cmdmescall@gmail.com>
2018-06-25 16:44:27 +0100

    minor changes to Nav and Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,14 +10,13 @@ class Footer extends Component {
             <p className="mh5">Account Settings</p>
           </div>
           <div className="openingHours fl w-30">
-            <p className="mh5">Username</p>
-            <p className="mh5">Account Settings</p>
+            <p className="mh5">Hours</p>
+            <p className="mh5">Don't bother, we're closed</p>
           </div>
           <div className="Map fl w-30">
-            <p className="mh5">Username</p>
-            <p className="mh5">Account Settings</p>
+            <p className="mh5">MAP GOES HERE</p>
           </div>
-          <div className="flex socials fl w-100">
+          <div className="flex socials mt3 fl w-100">
           <p>Instagram</p>
           <p>Facebook</p>
           <p>Twitter</p>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,13 +4,13 @@ import './Navbar.css';
 class Navbar extends Component {
   render() {
     return (
-        <nav>
+        <nav className="mb4">
           <div className="acctInfo fl w-30">
-            <p className="mh5">Username</p>
-            <p className="mh5">Account Settings</p>
+            <p className="mv3 mh5">Username</p>
+            <p className="mv1 mh5">Account Settings</p>
           </div>
           <div className="mainNav fl w-70">
-            <p className="br-pill inline-flex mh5 ba b--black pa3 ma">Order</p>
+            <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma">Order</p>
             <p className="br-pill inline-flex mh5 ba b--black pa3 ma">4 Points</p>
           </div>
         </nav>
