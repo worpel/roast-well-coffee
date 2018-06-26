435f8d0322218f15163bb1192fe142122b3ad40e
cmdmescall <cmdmescall@gmail.com>
2018-06-26 14:35:32 +0100

    Added routing for Home and Order, plus API for getting drinks and pastries to the Orders page

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css';
 
 class Navbar extends Component {
   render() {
+    const {route, onRouteChange} = this.props;
     return (
         <nav className="mb4">
           <div className="acctInfo fl w-30">
@@ -10,7 +11,11 @@ class Navbar extends Component {
             <p className="mv1 mh5">Account Settings</p>
           </div>
           <div className="mainNav fl w-70">
-            <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma">Order</p>
+            {route === 'home' ?
+            <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma" onClick={() => {onRouteChange('order')}}>Order</p>
+            :
+            <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma" onClick={() => {onRouteChange('home')}}>Home</p>
+            }
             <p className="br-pill inline-flex mh5 ba b--black pa3 ma">4 Points</p>
           </div>
         </nav>
