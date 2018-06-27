9825f27343eebfb5ceafb7e1d0f9f13442c61e87
Siân Rachel <sianracheldanforth@gmail.com>
2018-06-27 15:28:47 +0100

    layout changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -14,12 +14,15 @@ class Navbar extends Component {
           <img src={mainLogo} />
         </div>
         <div className="acctInfo fl w-20 tc">
+          <div />
           <p className="mv3 mh5">Username</p>
         </div>
         <div className="acctInfo fl w-20 tc">
+          <div />
           <p className="mv1 mh5">Account Settings</p>
         </div>
         <div className="mainNav tc fl w-40">
+          <div />
           {/* <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma"><Link to='/order'>Order</Link></p> */}
           {/* <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma"><Link to='/'>Home</Link></p> */}
           <p className="br-pill inline-flex mh5 pa3 ma">4 Points</p>
