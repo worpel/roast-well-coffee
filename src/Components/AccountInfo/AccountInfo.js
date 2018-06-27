e7195f662376f0386dbadbbf6116acc7a02916ef
Siân Rachel <sianracheldanforth@gmail.com>
2018-06-27 16:07:01 +0100

    account layout

--- src/Components/AccountInfo/AccountInfo.js
+++ src/Components/AccountInfo/AccountInfo.js
@@ -37,7 +37,7 @@ class AccountInfo extends Component {
             <div>Loading...</div>
         ) : (
                 <div className="accountInfo">
-                    <div className="messages mh5 fl w-100">
+                    <div className="messages fl w-100">
                         {this.state.type === 'personal' ? (
                             <div className="personal messages">
                                 {/* Personal Account */}
@@ -63,12 +63,10 @@ class AccountInfo extends Component {
                     {this.state.type === 'personal' ? (
                         <div>
                             {/* Personal Account */}
-                            <div className="rewardPoints mh2 fl w-50">
-                                <p>{this.state.rewardPoints}</p>
+                            <div className="history mh2 fl w-50" >
+                                <p>Personal history</p>
+                                <ul></ul>
                             </div>
-                            <div className="history mh2 fl w-50" />
-                            <p>Personal history</p>
-                            <ul></ul>
                         </div>
                     ) : (
                             <div>
