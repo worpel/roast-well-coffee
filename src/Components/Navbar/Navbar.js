5884d3a8059452a181b9abf88420ba4594fb3492
cmdmescall <cmdmescall@gmail.com>
2018-06-28 08:44:49 +0100

    Added Link to OurShops section of the site

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,35 +9,21 @@ class Navbar extends Component {
     return (
 
       <nav className="mb5">
-<<<<<<< HEAD
         <div className='javalogo fl w-20 tc'>
         <Link to='/'><img src={mainLogo} /></Link>
-=======
-        <div className='javalogo tr fl ph1 w-20 tc'>
-          <img src={mainLogo} />
->>>>>>> sianbranch
         </div>
         <div className="acctInfo tr fl ph1 w-20 tc">
           <div />
           <p className="mv3 mh5">Username</p>
+          <hr/>
+          <p className="br-pill flex tr mh5 pa3 ma">4 Points</p>
         </div>
-<<<<<<< HEAD
         <div className="acctInfo fl w-20 tc">
           <p className="mv1 mh5"><Link to='/account'>Account Settings</Link></p>
+          <p className="br-pill mv3 inline-flex mh5 ma"><Link to='/order'>Order</Link></p>
+          <p className="br-pill mv3 inline-flex mh5 ma"><Link to='/ourshops'>Our Shops</Link></p>
         </div>
         <div className="mainNav tc fl w-40">
-          <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma"><Link to='/order'>Order</Link></p>
-=======
-        <div className="acctInfo tr fl ph1 w-20 tc">
-          <div />
-          <p className="tr mv1 mh5">Account Settings</p>
-        </div>
-        <div className="mainNav tr fl ph1 w-40">
-          <div />
-          {/* <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma"><Link to='/order'>Order</Link></p> */}
->>>>>>> sianbranch
-          {/* <p className="br-pill mv3 inline-flex mh5 ba b--black pa3 ma"><Link to='/'>Home</Link></p> */}
-          <p className="br-pill flex tr mh5 pa3 ma">4 Points</p>
         </div>
       </nav>
     );
