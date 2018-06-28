5e32672a70574557af4ab74162bd2ace971343a4
cmdmescall <cmdmescall@gmail.com>
2018-06-28 13:53:28 +0100

    Have removed Font icons and dependencies as we have decided against using them

--- src/App.js
+++ src/App.js
@@ -15,14 +15,6 @@ import {
 } from 'react-router-dom';
 
 import tachyons from 'tachyons';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-
-
-
-library.add(fab, faStar);
 
 class App extends Component {
     constructor() {
