0129f7a1479215bb8bfe1d63c17fed50019941b3
cmdmescall <cmdmescall@gmail.com>
2018-06-28 10:39:41 +0100

    Added dynamic loading of history onto the page

--- src/Components/AccountInfo/AccountInfo.js
+++ src/Components/AccountInfo/AccountInfo.js
@@ -9,12 +9,13 @@ class AccountInfo extends Component {
             favDrink: '',
             rewardPoints: '',
             email: '',
-            type: ''
+            type: '',
+            history: []
         };
     }
     componentDidMount() {
-        const { fetchApi, userName } = this.props;
-        fetchApi('users/1')
+        const { fetchApi, currentUser } = this.props;
+        fetchApi(`users/${currentUser}`)
             .then(resp => resp.json())
             .then(data => {
                 const { name, type, favDrink, rewardPoints, email } = data[0];
@@ -29,70 +30,93 @@ class AccountInfo extends Component {
             .catch(err => {
                 console.log(err);
             });
+        fetchApi(`history/${currentUser}`)
+            .then(resp => resp.json())
+            .then(data => {
+                const { productsOrdered } = data[0];
+                this.setState({
+                    history: productsOrdered
+                });
+                this.state.history.forEach(el => {
+                    let newItem = document.createElement('div');
+                    newItem.innerHTML = `<p>${el.size ? el.size : ''} ${el.name} - £${el.price.toFixed(2)}`
+                    document.getElementById(`${this.state.type}History`).appendChild(newItem)
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
     render() {
         return !this.state.name ? (
             <div>Loading...</div>
         ) : (
-                <div className="accountInfo">
-                    <div className="messages fl w-100">
-                        {this.state.type === 'personal' ? (
-                            <div className="personal messages">
-                                {/* Personal Account */}
-                                <p>
-                                    Promo: Double Doughnut Wednesdays! Come in
-                                    between 1100 and 1300 Wednesdays for Buy One Get
-                                    One Free!
+            <div className="accountInfo">
+                <div className="messages fl w-100">
+                    {this.state.type === 'personal' ? (
+                        <div className="personal messages">
+                            {/* Personal Account */}
+                            <p>
+                                Promo: Double Doughnut Wednesdays! Come in
+                                between 1100 and 1300 Wednesdays for Buy One Get
+                                One Free!
                             </p>
-                                <p>
-                                    Also don't forget if you order ahead before 0900
+                            <p>
+                                Also don't forget if you order ahead before 0900
                                 you get 30% off a {this.state.favDrink}.
                             </p>
-                            </div>
-                        ) : (
-                                <div>
-                                    {/* Business Account */}
-                                    <div className="business messages">
-                                        <p>Business Account</p>
-                                    </div>
-                                </div>
-                            )}
-                    </div>
-                    {this.state.type === 'personal' ? (
+                        </div>
+                    ) : (
                         <div>
-                            {/* Personal Account */}
-                            <div className="history mh2 fl w-50" >
-                                <p>Personal history</p>
-                                <ul></ul>
+                            {/* Business Account */}
+                            <div className="business messages">
+                                <p>Business Account</p>
                             </div>
                         </div>
-                    ) : (
-                            <div>
-                                {/* Business Account */}
-                                <div className="history mh2 fl w-100"> </div>
-                                <p>Business History</p>
-                                <ul></ul>
-                            </div>
-                        )}
-                    {this.state.type === 'personal' ? (
-                        <div className="personaldetails fl w-100">
-                            Hey there {this.state.name}! Hope you're having an
-                            awesome day.
-                                <p className="details">
-                            </p>
-                            <p>Your registered e-mail is {this.state.email}</p>
-                        </div>
-                    ) : (
-                            <div className="business details fl w-100">
-                                <p>
-                                    Hey there {this.state.name}! Hope you're having an
-                                    awesome day!
-                        </p>
-                                <p>Your registered e-mail is {this.state.email}</p>
-                            </div>
-                        )}
+                    )}
                 </div>
-            );
+                {this.state.type === 'personal' ? (
+                    <div>
+                        {/* Personal Account */}
+                        <div
+                            id="personalHistory"
+                            className="history mh2 fl w-50"
+                        >
+                            <p>Personal history</p>
+                            <ul />
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        {/* Business Account */}
+                        <div
+                            id="businessHistory"
+                            className="history mh2 fl w-100"
+                        >
+                            {' '}
+                        </div>
+                        <p>Business History</p>
+                        <ul />
+                    </div>
+                )}
+                {this.state.type === 'personal' ? (
+                    <div className="personaldetails fl w-100">
+                        Hey there {this.state.name}! Hope you're having an
+                        awesome day.
+                        <p className="details" />
+                        <p>Your registered e-mail is {this.state.email}</p>
+                    </div>
+                ) : (
+                    <div className="business details fl w-100">
+                        <p>
+                            Hey there {this.state.name}! Hope you're having an
+                            awesome day!
+                        </p>
+                        <p>Your registered e-mail is {this.state.email}</p>
+                    </div>
+                )}
+            </div>
+        );
     }
 }
 
