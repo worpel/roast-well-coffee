48430db06c099b75231093aac5da211674f922b9
cmdmescall <cmdmescall@gmail.com>
2018-06-28 15:08:58 +0100

    Added Login page and changed the Navbar so it changes based on prop loggedIn

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,8 +4,12 @@ import './Login.css';
 class Login extends Component {
   render() {
     return (
-      <div className="flex">
-        
+      <div className="mv5 flex flex-column w-40 center">
+        <label htmlFor="email">Email</label>
+          <input className="mb4 mt2" name="email" type="email"/>
+          <label htmlFor="password">Password</label>
+          <input className="mb4 mt2" type="password" name="password" id="password"/>
+          <button className="center pv2 ph3 w-30 submit">Submit</button>
       </div>
     );
   }
